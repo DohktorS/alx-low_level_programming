e8e898f87293c2f911c14f0ac8715ea86efb0f0a
DohktorS <somtosherill66@gmail.com>
2022-12-14 04:13:22 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Is lower task
+ * @c: Hello betty
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
