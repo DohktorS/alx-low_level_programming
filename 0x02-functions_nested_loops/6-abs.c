a95845cef4c46982a77899c3e99592e9503b20c9
DohktorS <somtosherill66@gmail.com>
2022-12-14 05:06:00 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - No such thing as absolute
+ * @s: Task 6
+ * Return: 0
+ */
+
+int _abs(int s)
+{
+	if (s < 0)
+	{
+		return (s * -1);
+	}
+	else
+	{
+		return (s);
+	}
+}
