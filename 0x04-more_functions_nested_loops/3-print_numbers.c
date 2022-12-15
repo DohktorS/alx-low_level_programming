96d7898c5f71f1eb0b0dbb3630c3e1153530edf6
DohktorS <somtosherill66@gmail.com>
2022-12-15 13:18:53 -0800

    Task 4 nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 48; n < 58; n++)
 	{
-		_putchar(c);
+		_putchar(n);
 	}
-	_putchar('\n');
+	_putchar(10);
 }
