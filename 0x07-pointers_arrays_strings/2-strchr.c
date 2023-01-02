3c30f07b2cab0ae8361032e9774f828569dc3eec
DohktorS <somtosherill66@gmail.com>
2023-01-02 14:08:28 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: string
+ * @c: character to search
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (*s++);
+	return (NULL);
+}
