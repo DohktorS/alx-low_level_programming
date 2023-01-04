82e91d68ac4d2fa18333871a10194aaafc9ad9a9
DohktorS <somtosherill66@gmail.com>
2023-01-04 12:51:54 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Fx that returns x raised to the power of y
+ * @x: parameter x^y
+ * @y: power result
+ * Return: return if successful
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
