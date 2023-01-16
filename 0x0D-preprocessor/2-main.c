d4267bbb1bf9c0d6a68dae298eab505faa9ca684
DohktorS <somtosherill66@gmail.com>
2023-01-16 11:07:13 -0800

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Fx that prints the name of the file
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n",  __FILE__);
 	return (0);
 }
